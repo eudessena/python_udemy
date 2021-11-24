7ede663e17b9db03ddad5250f3b9b1693a24d64f
eudes sena <eudessena4@gmail.com>
2021-11-24 09:39:40 -0300

    atualização

---
+++ secao_2/Strings.py
@@ -0,0 +1,18 @@
+"""
+tudo em python é um objeto
+string tipo primitivo em python
+string(cadeia de caractere) no python - tudo que estiver entre aspas
+
+caractere de escape
+
+"""
+
+print('hello, "sena"')  # melhor usar essa maneira
+print("hello, 'eudes' ")
+print("hello, '\"eudes\" ")  # caractere de escape \ para o interpretador ignorar as aspas colocadas no eudes
+print('hello, \'eudes\' ')   # não é legal usar carectere de escape, somente se for extremamente necessário
+
+#  o interpretador ler a barra "\"
+
+print('eudes \n sena')  # quebra de linha \n
+print(r'eudes \n sena')  # o "r" diz para o interpretador nao executar nada logo o \n nao será executado
---
+++ secao_2/tipos_de_dados_primitivos.py
@@ -0,0 +1,24 @@
+"""
+tipos de dados
+
+str - string - "hello", 'hello'...
+int - inteiro - ... -1,0,1.........
+float/real - ... -1.0 , 0.0 , 1.0.........
+bool- booleano/lógico - testa a logica true/false
+
+obs - todo comando que usa () em python geralmente é função ou classe
+"""
+print('eudes', type('eudes'))
+print(7, type(7))
+print(7.0, type(7.0))
+print(7 == 7, type(7 == 7))
+
+# Casting - converter um tipo em outro tipo
+
+print('10', type(int('10')))
+print(10.10, type(int(10)))
+
+print('eudes sena', type('eudes sena'))
+print(24, type(24))
+print(1.67, type(1.67))
+print(24 > 18, type(24 > 18))
