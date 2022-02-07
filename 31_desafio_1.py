4a70f8c840babcbc4e9b928b82b9343cacfd8eba
eudes sena <eudessena4@gmail.com>
2022-02-07 01:18:03 -0300

    god is good

---
+++ 31_desafio_1.py
@@ -0,0 +1,32 @@
+"""
+contadores crescentes e decrescentes
+
+"""
+
+
+
+
+
+
+
+c = 0
+c1 = 10
+
+while c <= 8:
+
+   print(c)
+   c = c + 1
+
+   print(c1)
+   c1 = c1 - 1
+   
+for p, r in enumerate(range(10, 1, -1)):
+   print(p, r)        
+
+
+
+
+        
+
+
+    
